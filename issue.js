490dab8b126703cef2f2a5ae4218b17384e7859a
Md Shawon <shawonmohammad787@gmail.com>
2026-03-07 04:58:55 +0600

    color dynamic

--- issue.js
+++ issue.js
@@ -1,3 +1,31 @@
+
+
+const createElements = (arr) => {
+
+  const issueElements = arr.map((el, index) => {
+
+    if(index === 0){
+      return `
+      <span class="flex items-center gap-1 bg-red-200 text-red-600 px-2 py-1 rounded text-xs">
+        <img src="./assets/Vector (2).png" alt="">
+        ${el.toUpperCase()}
+      </span>`;
+    }
+
+    if(index === 1){
+      return `
+      <span class="flex items-center gap-1 bg-yellow-100 text-yellow-600 px-2 py-1 rounded text-xs">
+        <img src="./assets/Vector (3).png" alt="">
+        ${el.toUpperCase()}
+      </span>`;
+    }
+
+    return "";
+  });
+
+  return issueElements.join(" ");
+};
+
 const allIssues = document.getElementById("all-issues");
 
 fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
@@ -8,30 +36,41 @@ function displayIssues(issues){
     console.log(issues)
     allIssues.innerHTML = "";
     issues.forEach(issue =>{
+        let textColor = "";
+        let bgColor = "";
+        if(issue.priority == "high"){
+            textColor = "text-red-600";
+            bgColor = "bg-red-200";
+        };
+        if(issue.priority == "medium"){
+            textColor = "text-yellow-600";
+            bgColor = "bg-yellow-200";
+        };
+        if(issue.priority == "low"){
+            textColor = "text-black";
+            bgColor = "bg-slate-200";
+        };
         const card = document.createElement("div");
         card.className = "py-4 border border-gray-200 rounded-lg shadow-sm";
         card.innerHTML= `
         <div class="">
                 <div class="flex justify-between items-center px-4">
                     <img src="./assets/Open-Status.png" alt="">
-                    <p class="bg-red-200 py-1 px-3 font-medium text-xs text-red-600 rounded-md uppercase">${issue.priority}</p>
+                    <p class="${bgColor} py-1 px-3 font-medium text-xs rounded-md uppercase ${textColor}">${issue.priority}</p>
                 </div>
                 <h2 class="font-semibold text-xl mt-4 px-4">${issue.title}</h2>
                 <p class="text-gray-500 text-sm mt-1 px-4">${issue.description}</p>
                 <div class="flex items-center gap-3 mt-4 pb-4 border-b border-b-gray-300">
-                    <div class="flex items-center justify-center gap-1 bg-red-200 py-1 px-3 rounded-md ml-4">
-                        <img src="./assets/Vector (2).png" alt="">
-                        <p class="text-xs text-red-500 font-medium uppercase">${issue.labels[0]}</p>
-                    </div>
-                    <div class="flex items-center justify-center gap-1 bg-yellow-100 py-1 px-3 rounded-md mr-4">
-                        <img src="./assets/Vector (3).png" alt="">
-                        <p class="text-xs text-yellow-600 font-medium uppercase">${issue.labels[1]}</p>
-                    </div>
+                   <div class="px-4 flex gap-2">
+                   ${createElements(issue.labels)}
+                   </div>
                 </div>
                 <p class="text-sm text-gray-500 mt-3 px-4">${issue.author}</p>
                 <p class="text-sm text-gray-500 mt-1 px-4">${issue.createdAt}</p>
             </div>
         `;
+       
         allIssues.appendChild(card);
-    })
+    });
 }
+
