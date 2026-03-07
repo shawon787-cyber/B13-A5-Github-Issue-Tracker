7f6a200e2cc1da04e59c1773c1c7c05506dc94b6
Md Shawon <shawonmohammad787@gmail.com>
2026-03-07 16:05:29 +0600

    modal complete

--- issue.js
+++ issue.js
@@ -1,7 +1,15 @@
+const issueTitle = document.getElementById("issue-title");
+const issueModal = document.getElementById("issue_modal");
+const issueStatus = document.getElementById("issue-status");
+const authorName = document.getElementById("author-name");
+const createdDate = document.getElementById("created-date");
+const issueDescription = document.getElementById("issue-description");
+const writerName = document.getElementById("name");
+const priorityValue = document.getElementById("priority-value");
+const modalIssueLabels = document.getElementById("modal-issue-labels");
 
 
 const createElements = (arr) => {
-
   const issueElements = arr.map((el, index) => {
 
     if(index === 0){
@@ -33,9 +41,20 @@ fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
 .then(data => displayIssues(data.data))
 
 function displayIssues(issues){
+  
     console.log(issues)
     allIssues.innerHTML = "";
+    document.getElementById("tracker-length").innerText = issues.length;
     issues.forEach(issue =>{
+        
+
+        let border = "";
+        if(issue.status == "open"){
+            border = "border-green-500 border-t-4"
+        };
+        if(issue.status == "closed"){
+          border = "border-purple-500 border-t-4"
+        }
         let textColor = "";
         let bgColor = "";
         if(issue.priority == "high"){
@@ -51,17 +70,17 @@ function displayIssues(issues){
             bgColor = "bg-slate-200";
         };
         const card = document.createElement("div");
-        card.className = "py-4 border border-gray-200 rounded-lg shadow-sm";
+        card.className = `py-4 ${border} rounded-lg shadow-sm`;
         card.innerHTML= `
         <div class="">
                 <div class="flex justify-between items-center px-4">
                     <img src="./assets/Open-Status.png" alt="">
-                    <p class="${bgColor} py-1 px-3 font-medium text-xs rounded-md uppercase ${textColor}">${issue.priority}</p>
+                    <p onclick="openIssueModal(${issue.id})" class="${bgColor} py-1 px-3 font-medium text-xs rounded-md uppercase ${textColor} cursor-pointer">${issue.priority}</p>
                 </div>
                 <h2 class="font-semibold text-xl mt-4 px-4">${issue.title}</h2>
                 <p class="text-gray-500 text-sm mt-1 px-4">${issue.description}</p>
                 <div class="flex items-center gap-3 mt-4 pb-4 border-b border-b-gray-300">
-                   <div class="px-4 flex gap-2">
+                   <div onclick="showModal()" class="px-4 flex gap-2 ">
                    ${createElements(issue.labels)}
                    </div>
                 </div>
@@ -73,4 +92,50 @@ function displayIssues(issues){
         allIssues.appendChild(card);
     });
 }
+
+async function openIssueModal(issueId){
+  console.log(issueId);
+  const res =await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`);
+  const data =await res.json();
+  const issueDetails = data.data;
+  let textColor = "";
+  let bgColor = "";
+  if(issueDetails.status == "open"){
+    textColor = "text-white";
+    bgColor = "bg-green-600"
+  };
+  if(issueDetails.status == "closed"){
+    textColor = "text-white";
+    bgColor = "bg-red-600"
+  };
+  let priorityText = "";
+  let priorityBg = "";
+  if(issueDetails.priority == "high"){
+    priorityText = "text-white";
+    priorityBg = "bg-red-600"
+  };
+  if(issueDetails.priority == "medium"){
+    priorityText = "text-white";
+    priorityBg = "bg-yellow-600"
+  };
+  if(issueDetails.priority == "low"){
+    priorityText = "text-gray-800";
+    priorityBg = "bg-slate-200"
+  };
+  console.log(issueDetails);
+  issueModal.showModal();
+  issueTitle.textContent = issueDetails.title;
+  issueStatus.textContent = issueDetails.status.toUpperCase();
+  issueStatus.className = `py-1 px-3 rounded-sm ${bgColor} ${textColor}`;
+  authorName.textContent = issueDetails.author;
+  createdDate.textContent = issueDetails.createdAt;
+  issueDescription.textContent = issueDetails.description;
+  writerName.textContent = issueDetails.assignee;
+  priorityValue.textContent = issueDetails.priority.toUpperCase();
+  priorityValue.className = `text-sm text-gray-800 font-medium py-1 px-2 w-18 mt-1 text-center rounded-sm ${priorityBg} ${priorityText}`;
+  modalIssueLabels.innerHTML = createElements(issueDetails.labels);
+  
+}
+
+
 
